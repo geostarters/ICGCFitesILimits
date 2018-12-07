8fb8a3b254bf49b403d23bc2abbae7821540d8d4
Isaac Besora Vilardaga <isaac@altersport.cat>
2018-12-07 11:45:22 +0100

    Working with our Map component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { View } from 'react-native';
-import MapboxGL from '@mapbox/react-native-mapbox-gl';
+import { Map } from 'icgc-react-native-components';
+import { MapboxToken } from './constants'
 
 // styles
 import sheet from './styles/sheet';
 
-MapboxGL.setAccessToken("pk.eyJ1IjoiZ2Vvc3RhcnRlcnMiLCJhIjoiY2pvbnp4czhhMHoyZTNxdXVmaXE0cW9vcSJ9.EdP3QOltpjBJ-bWX3VVbkg");
 
 class App extends React.Component {
 
   render() {
     return (
       <View style={sheet.matchParent}>
-        <MapboxGL.MapView
-          showUserLocation={true}
-          zoomLevel={12}
-          userTrackingMode={MapboxGL.UserTrackingModes.Follow}
-          styleURL={ MapboxGL.StyleURL.Light }
-          style={sheet.matchParent}
+        <Map options = {{
+				showUserLocation: true,
+				zoomLevel: 12,
+				userTrackingMode: Map.UserTrackingModes.Follow,
+				styleURL: Map.StyleURL.Night 
+			}}
+			mapboxToken={ MapboxToken }
+			style={sheet.matchParent}
         />
       </View>
     );
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const MapboxToken = "pk.eyJ1IjoiZ2Vvc3RhcnRlcnMiLCJhIjoiY2pvbnp4czhhMHoyZTNxdXVmaXE0cW9vcSJ9.EdP3QOltpjBJ-bWX3VVbkg";
+
+export { MapboxToken }
